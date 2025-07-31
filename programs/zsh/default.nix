dd1d0c2f03eb200c2566c956511a9b9a1827edd6
DarJanu <jetti.janu@gmail.com>
2025-07-31 21:09:04 +0200

    12   2025-07-31 21:09:02   (current)

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,15 +1,14 @@
 {pkgs, ...}: {
   programs.zsh = {
     enable = true;
+    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     ohMyZsh = {
       enable = true;
-      theme = "agnoster";
+      #theme = "agnoster";
       plugins = [
         "git"
-        "zsh-syntax-highlighting"
-        "zsh-autosuggestions"
       ];
     };
   };
