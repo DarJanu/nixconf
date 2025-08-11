31e4c53df3bbc15f1f90edf1a992f481ece0daee
DarJanu <jetti.janu@gmail.com>
2025-08-11 17:02:27 +0200

    32   2025-08-11 17:02:26   (current)

--- dm/default.nix
+++ dm/default.nix
@@ -35,4 +35,21 @@
     layout = "at";
     variant = "";
   };
+  services.picom = {
+    enable = true;
+    fade = true;
+    #    vSync = true;
+    shadow = true;
+    fadeDelta = 4;
+    inactiveOpacity = 0.8;
+    activeOpacity = 1;
+    #    backend = "glx";
+    settings = {
+      blur = {
+        #method = "dual_kawase";
+        background = true;
+        strength = 5;
+      };
+    };
+  };
 }
