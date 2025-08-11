f4f82e840dd07c021aaed85ea07d2252ef8f5d7b
DarJanu <jetti.janu@gmail.com>
2025-08-11 18:03:01 +0200

    36   2025-08-11 18:02:59   (current)

--- programs/default.nix
+++ programs/default.nix
@@ -19,6 +19,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    acpi
+    alsa-utils
     bash
     wget
     powerline-fonts
