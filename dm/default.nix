761b82e349cd44bb2a8e64b82d3cb55669ed61d9
DarJanu <jetti.janu@gmail.com>
2025-08-12 16:10:10 +0200

    43   2025-08-12 16:10:08   (current)

--- dm/default.nix
+++ dm/default.nix
@@ -14,18 +14,10 @@
       slurp
       dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
     ];
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-    '';
   };
 
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
+  services.displayManager.sddm.wayland.enable = true;
   programs.waybar.enable = true;
 
   # Configure keymap in X11
