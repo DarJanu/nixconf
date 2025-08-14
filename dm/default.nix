2d9d26c943f8dbf33dfe55aec7ac934b21be6a5f
DarJanu <jetti.janu@gmail.com>
2025-08-14 18:13:15 +0200

    52   2025-08-14 18:08:24   (current)

--- dm/default.nix
+++ dm/default.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   services.dbus.enable = true;
-
+  hardware.graphics.enable = true;
   environment.systemPackages = with pkgs; [
     dmenu
     grim # screenshot functionality
@@ -22,8 +22,26 @@
     enable = true;
     wrapperFeatures.gtk = true;
   };
-
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  # kanshi systemd service
+  systemd.user.services.kanshi = {
+    description = "kanshi daemon";
+    environment = {
+      WAYLAND_DISPLAY = "wayland-1";
+      DISPLAY = ":0";
+    };
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+    };
+  };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "jetti";
+      };
+    };
+  };
   programs.waybar.enable = true;
 }
