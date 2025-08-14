febc12e34f36370fc325434de6afcba27953e7cc
DarJanu <jetti.janu@gmail.com>
2025-08-14 18:16:22 +0200

    53   2025-08-14 18:16:20   (current)

--- dm/default.nix
+++ dm/default.nix
@@ -3,37 +3,8 @@
   pkgs,
   ...
 }: {
-  services.dbus.enable = true;
-  hardware.graphics.enable = true;
-  environment.systemPackages = with pkgs; [
-    dmenu
-    grim # screenshot functionality
-    slurp # screenshot functionality
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-    mako # notification system developed by swaywm maintainer
-  ];
+  programs.hyprland.enable = true;
 
-  # Enable the gnome-keyring secrets vault.
-  # Will be exposed through DBus to programs willing to store secrets.
-  services.gnome.gnome-keyring.enable = true;
-
-  # enable Sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-  # kanshi systemd service
-  systemd.user.services.kanshi = {
-    description = "kanshi daemon";
-    environment = {
-      WAYLAND_DISPLAY = "wayland-1";
-      DISPLAY = ":0";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
-    };
-  };
   services.greetd = {
     enable = true;
     settings = {
