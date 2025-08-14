94b559bba2f6ecf455396be587575d908fefc9d9
DarJanu <jetti.janu@gmail.com>
2025-08-14 17:34:20 +0200

    48   2025-08-14 17:21:04   (current)

--- dm/default.nix
+++ dm/default.nix
@@ -7,11 +7,12 @@
 
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+    wlr.enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-wlr
     ];
-    xdgOpenUsePortal = true;
   };
 
   programs.sway = {
