912fa61a1ce37c085e5c9a0a4a7e5ea5423de21e
DarJanu <jetti.janu@gmail.com>
2025-09-21 18:15:55 +0200

    update

--- dm/default.nix
+++ dm/default.nix
@@ -3,20 +3,24 @@
   pkgs,
   ...
 }: {
-  programs.hyprland.enable = true;
-  environment.systemPackages = with pkgs; [
-    mako
-    hyprpolkitagent
-    hyprcursor
-  ];
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
-        user = "jetti";
-      };
-    };
-  };
-  programs.waybar.enable = true;
+  services.xserver.enable = true; # optional
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  #  programs.hyprland.enable = true;
+  #  environment.systemPackages = with pkgs; [
+  #    mako
+  #    hyprpolkitagent
+  #    hyprcursor
+  #  ];
+  #  services.greetd = {
+  #    enable = true;
+  #    settings = {
+  #      default_session = {
+  #        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
+  #        user = "jetti";
+  #      };
+  #    };
+  #  };
+  #  programs.waybar.enable = true;
 }
