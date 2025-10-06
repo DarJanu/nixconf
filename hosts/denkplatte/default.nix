e88b13c5689ddc5d19f81db42c84c33652e06d44
DarJanu <jetti.janu@gmail.com>
2025-10-06 22:35:48 +0200

    76   2025-10-06 22:35:46   (current)

--- hosts/denkplatte/default.nix
+++ hosts/denkplatte/default.nix
@@ -65,7 +65,8 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
+  services.openssh.allowSFTP = true;
+  services.openssh.settings.PermitRootLogin = "yes";
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
