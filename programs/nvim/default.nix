2b05795f2f53a4d38653ee066dd71b7db1afb6b6
DarJanu <jetti.janu@gmail.com>
2025-10-16 09:32:14 +0200

    12   2025-10-16 09:32:13   (current)

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -10,7 +10,7 @@
       '';
 
       packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [fugitive nerdtree vim-airline nvim-treesitter]; # Loaded automatically
+        start = [fugitive nerdtree vim-airline nvim-treesitter mason-nvim nvim-lspconfig]; # Loaded automatically
         opt = []; # Optional plugins (none here)
       };
     };
