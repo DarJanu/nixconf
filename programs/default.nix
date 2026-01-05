d8b1e2646bd494187bc9d932fd9cc59d4dd56710
DarJanu <jetti.janu@gmail.com>
2026-01-05 15:50:46 +0100

    53   2026-01-05 15:45:16   (current)

--- programs/default.nix
+++ programs/default.nix
@@ -27,8 +27,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   services.tailscale.enable = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
   environment.systemPackages = with pkgs; [
     #random stuffs
     thonny
