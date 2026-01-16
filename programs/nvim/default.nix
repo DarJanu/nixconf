aeb8985c4351391ee00d500f09844f7222349f13
DarJanu <jetti.janu@gmail.com>
2026-01-16 11:42:24 +0100

    65   2026-01-16 11:42:21   (current)

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -11,19 +11,7 @@
 
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [
-          fzf-lua
-          fzf-lsp-nvim
-          hex-nvim
-          fugitive
-          nerdtree
           vim-airline
-          nvim-treesitter
-          mason-nvim
-          nvim-lspconfig
-          nvim-treesitter-parsers.nasm
-          nvim-treesitter-parsers.c
-          nvim-treesitter-parsers.python
-          nvim-treesitter-parsers.nix
         ]; # Loaded automatically
         opt = []; # Optional plugins (none here)
       };
