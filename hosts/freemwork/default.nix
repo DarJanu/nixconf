2afa00e221b7ece229ae38471a3b042289276c1c
DarJanu <jetti.janu@gmail.com>
2026-01-28 08:10:45 +0100

    76   2026-01-28 08:10:42   (current)

--- hosts/freemwork/default.nix
+++ hosts/freemwork/default.nix
@@ -14,6 +14,7 @@
   boot.initrd.kernelModules = ["pinctrl_tigerlake"];
   networking.hostName = "freemwork"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostId = "deadbeef";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
