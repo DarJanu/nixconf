946f9050ea58284828adbb645d3961745251bb4b
DarJanu <jetti.janu@gmail.com>
2026-01-28 08:54:52 +0100

    78   2026-01-28 08:54:49   (current)

--- programs/arduinotools/default.nix
+++ programs/arduinotools/default.nix
@@ -2,4 +2,10 @@
   environment.systemPackages = [
     pkgs.arduino-ide
   ];
+  services.udev.extraRules = ''
+    # Arduino Portenta H7 DFU mode
+    ATTRS{idVendor}=="2341", ATTRS{idProduct}=="035b", MODE="0666"
+    # STM32 DFU (general)
+    ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="0666"
+  '';
 }
